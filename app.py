c66808d1c4081dacd3bd6c58527869de30348290
Joo Wan Lim <joowan980330@gmail.com>
2025-08-12 10:36:43 +0900

    Deployed the application and saved the url

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ import streamlit as st
 from PIL import Image
 from utils import load_model, stylize_image
 
+# https://jwlstyletransferapp.streamlit.app/
+
 st.set_page_config(page_title='Style Transfer APP', layout='centered')
 
 st.title('Neural Style Transfer')
